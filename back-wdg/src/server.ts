784103aed6f151b712b5631d540aa1e71d2629d5
ACigaran <agustincigaran@icloud.com>
2026-07-10 13:30:50 -0300

    server levatnando

--- back-wdg/src/server.ts
+++ back-wdg/src/server.ts
@@ -1,5 +1,5 @@
-import autoLoad from '@fastify/autoload';
 import fastify from 'fastify';
+import autoLoad from '@fastify/autoload';
 import cors from '@fastify/cors'
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
@@ -10,7 +10,7 @@ const server = fastify({logger:true}).withTypeProvider<TypeBoxTypeProvider>();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const front_port = Number(process.env.API_PORT) || 3000;
+const front_port = Number(process.env.API_PORT) || 4200;
 
 const origin = `http://localhost:${front_port}`
 await server.register(cors, {
@@ -30,10 +30,10 @@ await server.register(autoLoad, {
     dir: join(__dirname, 'routes'),
     routeParams: true,
 })
-
+const puerto_back = 6200;
 try{
-    await server.listen({ port: 3000, host: '::'});
-    console.log('Backend en linea - http://localhost:3000');
+    await server.listen({ port: puerto_back, host: '::'});
+    console.log(`Backend en linea - http://localhost:${puerto_back}`);
 }catch(err){
     server.log.error(err);
     process.exit(1);
